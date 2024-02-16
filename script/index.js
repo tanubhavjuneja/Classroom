becad611fc6cc52bcf1e958e524227541ff6bd71
tanubhavjuneja <119427827+tanubhavjuneja@users.noreply.github.com>
2024-02-17 00:56:29 +0530

    Update index.js

--- script/index.js
+++ script/index.js
@@ -1,16 +1,3 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-            var response = JSON.parse(xhr.responseText);
-            var username = response.username;
-            console.log('Username:', username);
-            document.getElementById('usernameLabel').textContent = username;
-        }
-    };
-    xhr.open('GET', '/get_username', true);
-    xhr.send();
-});
 function navigateTo(extension) {
     var url = '/' + extension;
     window.location.href = url;
